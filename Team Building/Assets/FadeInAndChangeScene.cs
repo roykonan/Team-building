b6ae0b8e68051fa236bf0b96535433b73f91fee6
BMarcelus <bmdizon@uci.edu>
2020-08-01 15:31:56 -0700

    gameOver

---
+++ Team Building/Assets/FadeInAndChangeScene.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class FadeInAndChangeScene : MonoBehaviour
+{
+    public float time;
+    private float timer;
+    public string sceneName;
+    public Image image;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        image.color = new Color(image.color.r,image.color.g,image.color.b,timer/time);
+        if(timer>=time) {
+          SceneManager.LoadScene(sceneName);
+        }
+    }
+}
